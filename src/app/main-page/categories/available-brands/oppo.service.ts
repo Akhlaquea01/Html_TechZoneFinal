65c0c8ae282178d6e4fc1281c784e5ccdfae36b2
Ashfaque <ashfaque386@gmail.com>
2021-07-11 15:14:22 +0530

    Created services for Samsung and Oppo

---
+++ src/app/main-page/categories/available-brands/oppo.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProducts } from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MobileService {
+  public _url: string = "/assets/data/oppo.json"
+  constructor(public http: HttpClient) { }
+  getMobiles(): Observable<IProducts[]> {
+    return this.http.get<IProducts[]>(this._url)
+  }
+}
