264362bda7f4e1c97328000d0fb7331e37d032fd
Akhlaque <akhlaquea01@gmail.com>
2022-10-28 20:23:48 +0530

    HomePage Css Updated

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,18 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  shopName:String="Tech Zone";
-  tagLine1:String="Get all the trending stuff";
-  tagLine2:String="Everything at an affordable price";
-  homePageOptions=[
-    
-      {route:'/main',icon:'fa fa-universal-access',name:'Welcome'},
-      {route:'/about',icon:'fa-bullseye',name:'About'},
-      {route:'/services',icon:'fa fa-cogs',name:'Admin'},
-      {route:'/user',icon:'fa fa-user',name:'Login'},
-      {route:'/products',icon:'fa fa-braille',name:'Products'},
-      {route:'/feedback',icon:'fa fa-clipboard',name:'Feedback'},
-      {route:'/contact',icon:'fa fa-map-signs',name:'Contacts'}
+  shopName: String = "Tech Zone";
+  tagLine1: String = "Get all the trending stuff";
+  tagLine2: String = "Everything at an affordable price";
+  homePageOptions = [
+    { route: '/main', icon: 'fa fa-universal-access', name: 'Welcome' },
+    { route: '/about', icon: 'fa-bullseye', name: 'About' },
+    { route: '/services', icon: 'fa fa-cogs', name: 'Admin' },
+    { route: '/user', icon: 'fa fa-user', name: 'Login' },
+    { route: '/products', icon: 'fa fa-braille', name: 'Products' },
+    { route: '/feedback', icon: 'fa fa-clipboard', name: 'Feedback' },
+    { route: '/contact', icon: 'fa fa-map-signs', name: 'Contacts' }
   ]
 
   constructor() { }
