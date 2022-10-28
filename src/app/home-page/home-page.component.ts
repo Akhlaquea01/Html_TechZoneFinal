815e5f3ddee5fb63dbaae052292b4934b31c1d42
Akhlaque <akhlaquea01@gmail.com>
2022-10-28 20:04:13 +0530

    Updated Home Page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -9,6 +9,16 @@ export class HomePageComponent implements OnInit {
   shopName:String="Tech Zone";
   tagLine1:String="Get all the trending stuff";
   tagLine2:String="Everything at an affordable price";
+  homePageOptions=[
+    
+      {route:'/main',icon:'fa fa-universal-access',name:'Welcome'},
+      {route:'/about',icon:'fa-bullseye',name:'About'},
+      {route:'/services',icon:'fa fa-cogs',name:'Admin'},
+      {route:'/user',icon:'fa fa-user',name:'Login'},
+      {route:'/products',icon:'fa fa-braille',name:'Products'},
+      {route:'/feedback',icon:'fa fa-clipboard',name:'Feedback'},
+      {route:'/contact',icon:'fa fa-map-signs',name:'Contacts'}
+  ]
 
   constructor() { }
   ngOnInit(): void {
